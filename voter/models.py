ed9e2638528419bec530085f6311c0b9d43fa9d1
kevin-kili <kilikevin435@gmail.com>
2020-06-08 16:11:05 +0300

    added ratings in models.py

--- voter/models.py
+++ voter/models.py
@@ -1,15 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 import datetime as dt
 
-# Create your models here.
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile' )
-    last_name = models.CharField(max_length =30)
-    email = models.EmailField()
-
+    profile_picture = models.ImageField(upload_to='images/', default='default.png')
+    bio = models.TextField(max_length=500, default="My Bio",  blank=True)
+    name = models.CharField(max_length=120, blank=True)
+    location = models.CharField(max_length=120, blank=True)
+    contact = models.CharField(max_length=120, blank=True)
+    
+    
     def __str__(self):
-        return f'{self.user.username} Pofile'
+        return f'{self.user.username} Profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+
 
 
 class Post(models.Model):
@@ -17,6 +34,62 @@ class Post(models.Model):
     url = models. CharField(max_length=255)
     description = models.TextField(max_length=500)
     technologies = models.CharField(max_length=255, blank=True)
-
+    photo = models.ImageField( blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    date = models.DateField(auto_now_add=True, blank=True)
+    
+    
+    
     def __str__(self):
         return f'{self.title}'
+
+    def delete_post(self):
+        self.delete()
+
+    @classmethod
+    def search_project(cls, title):
+        return cls.objects.filter(title__icontains=title).all()
+
+    @classmethod
+    def all_posts(cls):
+        posts = Post.objects.all()
+        return posts
+
+    def save_post(self):
+        self.save()
+        
+    
+class Rating(models.Model):
+    rating = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10'),
+    )
+
+    design = models.IntegerField(choices=rating, default=0, blank=True)
+    usability = models.IntegerField(choices=rating, blank=True)
+    content = models.IntegerField(choices=rating, blank=True)
+    score = models.FloatField(default=0, blank=True)
+    design_average = models.FloatField(default=0, blank=True)
+    usability_average = models.FloatField(default=0, blank=True)
+    content_average = models.FloatField(default=0, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='rater')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='ratings', null=True)
+
+    def save_rating(self):
+        self.save()
+
+    @classmethod
+    def get_ratings(cls, id):
+        ratings = Rating.objects.filter(post_id=id).all()
+        return ratings
+
+    def __str__(self):
+        return f'{self.post} Rating'
