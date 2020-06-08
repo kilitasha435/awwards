d3f669eead48c35d9b6580fb2deb27efe070ab7c
kevin-kili <kilikevin435@gmail.com>
2020-06-08 16:09:59 +0300

    added forms.py

---
+++ voter/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Post, Profile, Rating
+
+class PostForm(forms.ModelForm):
+    photo =forms.ImageField()
+    
+    class Meta:
+        model = Post
+        fields = ('photo','title', 'url', 'description', 'technologies')
+        
+        
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'location', 'profile_picture', 'bio', 'contact']
+
+
+class RatingsForm(forms.ModelForm):
+    class Meta:
+        model = Rating
+        fields = ['design', 'usability', 'content']
