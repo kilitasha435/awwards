6125550d2d1cd3fb87eba422313ee787f845b1ed
kevin-kili <kilikevin435@gmail.com>
2020-06-08 12:57:22 +0300

    added profile and post

--- voter/models.py
+++ voter/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile' )
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.user.username} Pofile'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    url = models. CharField(max_length=255)
+    description = models.TextField(max_length=500)
+    technologies = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f'{self.title}'
--- voter/tests.py
+++ voter/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from . models import *
 
 # Create your tests here.
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(id = 1, username = 'Brian', password = 'beta')
+        self.user.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user, User))
